c673da2f255a73b41ecaea2f6cd4b2233995e0da
leon guilherme <leonlopes1106@gmail.com>
2022-07-28 10:39:59 -0300

    criado componente nav e select das moedas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/templates/header/header.component';
+import { NavComponent } from './components/templates/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, NavComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
